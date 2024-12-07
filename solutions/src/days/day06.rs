443cd4d08d5ac611578b0b6e2e52428b56875e98
MechanicalMann <michael@mechmann.net>
2024-12-06 17:04:41 -0800

    Day 6 part 2 done with the brutest of force

--- solutions/src/days/day06.rs
+++ solutions/src/days/day06.rs
@@ -4,12 +4,12 @@ use std::{
 };
 
 use crate::{common::IPoint, solver::Solver};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 pub struct Solution;
 impl Solver<usize, usize> for Solution {
     const DAY: u8 = 06;
-    const TITLE: &'static str = "UNKNOWN";
+    const TITLE: &'static str = "Guard Gallivant";
 
     fn new() -> Self {
         Solution {}
@@ -17,15 +17,16 @@ impl Solver<usize, usize> for Solution {
 
     fn part_one(&self) -> Result<usize> {
         let map = self.input().get_as::<LabMap>()?;
-        Ok(map.walk_guard().len())
+        map.walk_guard()
     }
 
     fn part_two(&self) -> Result<usize> {
-        Ok(0)
+        let map = self.input().get_as::<LabMap>()?;
+        map.find_possible_obstructions()
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Tile {
     Clear,
     Blocked,
@@ -62,28 +63,61 @@ impl FromStr for LabMap {
     }
 }
 impl LabMap {
-    fn walk_guard(&self) -> HashSet<IPoint> {
-        let mut visited = HashSet::new();
-        let mut cur = self.guard_start;
-        let mut offset = 0;
-        loop {
-            let next = cur + OFFSETS[offset];
-            visited.insert(cur);
-            if let Some(np) = self.tiles.get(&next) {
-                match np {
-                    Tile::Blocked => {
-                        offset = (offset + 1) % 4;
-                        continue;
-                    }
-                    Tile::Clear => (),
-                }
-                cur = next;
-            } else {
-                break;
-            }
-        }
-        visited
+    fn walk_guard(&self) -> Result<usize> {
+        let path = do_walk(&self.tiles, self.guard_start)?;
+        Ok(path.len())
     }
+
+    fn find_possible_obstructions(&self) -> Result<usize> {
+        let obstructions = find_possible_obstructions(&self.tiles, self.guard_start)?;
+        Ok(obstructions.len())
+    }
+}
+
+fn do_walk(
+    tiles: &HashMap<IPoint, Tile>,
+    start: IPoint,
+) -> Result<HashMap<IPoint, HashSet<usize>>> {
+    let mut visited = HashMap::new();
+    let mut cur = start;
+    let mut offset = 0;
+    loop {
+        let next = cur + OFFSETS[offset];
+        let offsets = visited.entry(cur).or_insert(HashSet::new());
+        if !offsets.insert(offset) {
+            return Err(anyhow!("Caught in a loop!"));
+        }
+        if let Some(np) = tiles.get(&next) {
+            match np {
+                Tile::Blocked => {
+                    offset = (offset + 1) % 4;
+                    continue;
+                }
+                Tile::Clear => (),
+            }
+            cur = next;
+        } else {
+            break;
+        }
+    }
+    Ok(visited)
+}
+
+fn find_possible_obstructions(tiles: &HashMap<IPoint, Tile>, start: IPoint) -> Result<Vec<IPoint>> {
+    let mut obstructions = vec![];
+    // Let's brute force it babey
+    let optimal_path = do_walk(tiles, start)?;
+    for &visited in optimal_path.keys() {
+        if visited == start {
+            continue;
+        }
+        let mut alt_tiles = tiles.clone();
+        alt_tiles.entry(visited).and_modify(|t| *t = Tile::Blocked);
+        if do_walk(&alt_tiles, start).is_err() {
+            obstructions.push(visited);
+        }
+    }
+    Ok(obstructions)
 }
 
 #[cfg(test)]
@@ -122,14 +156,14 @@ mod tests {
     #[test]
     fn should_walk_path() -> Result<()> {
         let map = "#..\n..#\n^..".parse::<LabMap>()?;
-        let path = map.walk_guard();
+        let path = do_walk(&map.tiles, map.guard_start)?;
         assert_eq!(4, path.len());
         assert_eq!(
-            HashSet::from([
-                IPoint::new(0, 2),
-                IPoint::new(0, 1),
-                IPoint::new(1, 1),
-                IPoint::new(1, 2)
+            HashMap::from([
+                (IPoint::new(0, 2), HashSet::from([0])),
+                (IPoint::new(0, 1), HashSet::from([0])),
+                (IPoint::new(1, 1), HashSet::from([1])),
+                (IPoint::new(1, 2), HashSet::from([2]))
             ]),
             path
         );
@@ -139,8 +173,17 @@ mod tests {
     #[test]
     fn should_solve_part1() -> Result<()> {
         let map = EXAMPLE_INPUT.parse::<LabMap>()?;
-        let path = map.walk_guard();
-        assert_eq!(41, path.len());
+        let path = map.walk_guard()?;
+        assert_eq!(41, path);
+        Ok(())
+    }
+
+    #[test]
+    fn should_find_obstructions() -> Result<()> {
+        let map = ".#..\n...#\n.^..\n..#.".parse::<LabMap>()?;
+        let obstructions = find_possible_obstructions(&map.tiles, map.guard_start)?;
+        assert_eq!(1, obstructions.len());
+        assert_eq!(vec![IPoint::new(0, 2)], obstructions);
         Ok(())
     }
 }
